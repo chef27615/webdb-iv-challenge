10fa54960818d684fe96c4acc6dcf0572169e2a3
wei du <weidu@weis-Mac-mini.hsd1.va.comcast.net>
2019-05-09 22:37:01 -0400

    attempt join table

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -5,8 +5,11 @@ module.exports = {
     addRecipe
 }
 
-function getRecipes(){
-    return db('recipes')
+function getRecipes(dishId){
+    return db('recipes as r')
+    .join('dishes as d', 'r.dish_id', 'd.id')
+    .select('r.id', 'r.name', 'd.id', 'd.name')
+    .where({dish_id: dishId});
 }
 
 function addRecipe(recipe){
